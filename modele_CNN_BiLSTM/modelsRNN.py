56f1595d529bd94a251ad21a0bd719f487d64606
mateoisabey <77398592+mateoisabey@users.noreply.github.com>
2024-12-18 20:36:08 -0500

    Update modelsRNN.py

--- modele_CNN_BiLSTM/modelsRNN.py
+++ modele_CNN_BiLSTM/modelsRNN.py
@@ -6,12 +6,10 @@ class SimpleLSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
         super(SimpleLSTM, self).__init__()
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)  # Couche de sortie pour la classification
+        self.fc = nn.Linear(hidden_dim, output_dim)
     
     def forward(self, x):
-        # LSTM expects inputs of shape (batch_size, sequence_length, input_dim)
         lstm_out, _ = self.lstm(x)
-        # We only take the output of the last time step for classification
         final_out = lstm_out[:, -1, :]
         output = self.fc(final_out)
         return output
